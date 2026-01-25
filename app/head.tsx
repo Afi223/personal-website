acf8eba5a0b28ab93747b47db9e903d0abb2620b
Afi Hassan <afi.hassan13@gmail.com>
2026-01-25 16:06:05 -0500

    hello

--- app/head.tsx
+++ app/head.tsx
@@ -4,8 +4,8 @@ export default function Head() {
       <title>Abdiqani Hassan</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Abdiqani Hassan Portfolio Website" />
-      {/* Add a version to bust cache */}
-      <link rel="icon" type="image/png" href="/favicon.png?v=2" />
+      {/* Use SVG favicon with version query to bust cache */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
     </>
   )
 }
